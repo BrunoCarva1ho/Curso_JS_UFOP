9a089a8071650e06eed20106155584bdd4169512
BrunoCarva1ho <bruncrvl@gmail.com>
2023-12-11 21:26:02 -0300

    create

---
+++ exercicio1.js
@@ -0,0 +1,31 @@
+const readline = require('readline');
+
+
+const input = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+
+function conversor() {
+  input.question('Insira um valor em metros: ', (
+    input) => { let metros = parseFloat(input);
+
+    if (!isNaN(metros)) {
+      let decimetros = metros * 10;
+      let centimetros = metros * 100;
+      let milimetros = metros * 1000;
+
+      console.log(metros + ' metros é igual a:');
+      console.log(decimetros + ' decímetros');
+      console.log(centimetros + ' centímetros');
+      console.log(milimetros + ' milímetros');
+    } else {
+      console.log('Valor inválido, tente novamente.');
+    }
+
+    input.close();
+  });
+}
+
+conversor();
---
+++ exercicio2.js
@@ -0,0 +1,4 @@
+
+let numb = Math.floor(Math.random() * 100) + 1;
+
+console.log(numb);
