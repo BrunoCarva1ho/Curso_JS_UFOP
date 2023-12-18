48b25521488c835d289b454e56a77aac74bd4d81
BrunoCarva1ho <brunowp01@gmail.com>
2023-12-18 10:41:12 -0300

    up

--- Introducao/exercicio2.js
+++ Introducao/exercicio2.js
@@ -2,5 +2,5 @@
 let numb = Math.floor(Math.random() * 100) + 1;
 
 console.log("numero: ", numb);
-console.log("sucessor:", (numb+1));
-console.log("antecessor:", (numb-1));
+console.log("sucessor: ", (numb+1));
+console.log("antecessor: ", (numb-1));
