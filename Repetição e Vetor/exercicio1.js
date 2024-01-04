3f0b9e1219737677e043ae9d0b5e8fa5ef0067cb
BrunoCarva1ho <brunowp01@gmail.com>
2024-01-04 11:14:00 -0300

    up

---
+++ Repetição e Vetor/exercicio1.js
@@ -0,0 +1,24 @@
+
+var vetor = [];
+tam=10;
+
+//Insere números no vetor
+for(i=0; i<tam; i++){
+    vetor[i] = Math.floor(Math.random() * 100);
+}
+
+var maior = vetor[0];
+var menor = vetor[0];
+
+for(i=0;i<tam;i++){
+    console.log(vetor[i]);
+    if(vetor[i] > maior){
+        maior=vetor[i];
+    }
+    if(vetor[i] < menor){
+        menor=vetor[i];
+    }
+}
+
+console.log("Maior: ", maior);
+console.log("Menor: ", menor);
