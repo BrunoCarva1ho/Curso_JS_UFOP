adfce7505138e61c51fc1ecfd2254be3cdbe629a
BrunoCarva1ho <brunowp01@gmail.com>
2024-04-08 09:50:03 -0300

    end

--- Programação Assíncrona/exercicio1.js
+++ Programação Assíncrona/exercicio1.js
@@ -1,21 +1,8 @@
-var readline = require('prompt-sync')//--global
-var input = readline();
-
 import axios from "axios"
-
 
 async function api(){
     const resposta = await axios.get(`https://api.adviceslip.com/advice`)
     console.log(resposta.data.slip.advice)
 }
 
-
-do{
-    console.log("Digite 2 para receber um conselho, e 1 para sair:")
-    r = input()
-    
-    if(r == 2){
-        api()
-    }
-    
-}while(r != 1)
+api()
